fa75a1b2a8b92801eee15cc02640b1e6d73901e1
Angel Velasquez <pcisavel@upc.edu.pe>
2023-08-28 20:34:17 -0500

    feat(sales-order): added sales order aggregate.

---
+++ src/sales/domain/model/sales-order.js
@@ -0,0 +1,25 @@
+class SalesOrder {
+    constructor(id, customerId) {
+        this._id = id;
+        this.customerId = customerId;
+        this._items = [];
+
+    }
+    addItem({ productId, quantity, unitPrice }) {
+        this._items.push(new SalesOrderItem({
+            orderId: this._id,
+            productId,
+            quantity,
+            unitPrice
+        }));
+    }
+
+    calculateTotalPrice() {
+        return this._items.reduce((total, item) => total + item.calculateItemPrice(), 0);
+    }
+
+    get items() {
+        return this._items;
+    }
+
+}
